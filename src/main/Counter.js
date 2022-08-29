007fb26c74d24af57aa9ab28442adca522a36e6a
jackSparrow381 <developerasad381@gmail.com>
2022-08-29 09:46:01 +0500

    Added Counter functionality

---
+++ src/main/Counter.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Counter = () => {
+    const [count, setCount] = React.useState(0)
+  return (
+    <div>
+        <h1>Counter</h1>
+        <h2>{count}</h2>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
+        
+      
+    </div>
+  )
+}
+
+export default Counter
